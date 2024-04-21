b37640d8204673b40107de6d6a09b8899eacf9b8
Miguel <miguelpsanchis@gmail.com>
2024-04-21 21:21:41 +0200

    feat(rb): guessing game first version working

--- rust-book/guessing-game/README.md
+++ rust-book/guessing-game/README.md
@@ -11,3 +11,15 @@ The `Result` object has an `expect` method that causes the program to crash if t
 ## References
 
 The `&` symbol represents a reference, so that the data is not copied in memory again. However, references are immutable by default the same ways values are. Therefore, `&mut guess` passes a mutable reference.
+
+## Crates
+
+There are two kinds:
+* library crates (e.g. guessing game)
+* binary crates (e.g. "rand")
+
+By default, crates in the `[dependencies]` are internally prepended with `^`, so that updates (fixes) can be pulled.
+However, Cargo creates a `Cargo.lock` as well to keep the same versions in the future.
+
+Cargo can open the docs of imported libraries with:
+* `cargo doc --open`
--- rust-book/guessing-game/src/main.rs
+++ rust-book/guessing-game/src/main.rs
@@ -1,15 +1,38 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
-    println!("Input your guess.");
+  println!("** Welcome to the number guessing game! **\n");
+  let secret_number = rand::thread_rng().gen_range(1..=100);
+  println!("The secret number is: {secret_number}");
 
+  loop {
+    println!("Please input your guess.");
     let mut guess = String::new(); // mutable variable (vars are immutable by default)
-
+  
     // https://doc.rust-lang.org/std/io/struct.Stdin.html
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
+  
+    // variable shadowing => same name, used to change type but keeping name
+    let guess: u32 = match guess.trim().parse() {
+      Ok(foo) => foo,
+      Err(_) => {
+        println!("Your input must be a number. Try again...");
+        continue;
+      }
+    };
+  
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      }
+    }
+  }
+  
 }
