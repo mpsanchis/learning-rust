f0b5ac7b28096e05e3360fa2a311fd2c182e59f3
mpsanchis <miguelpsanchis@gmail.com>
2025-03-12 18:13:25 +0100

    chore(rb): Dereference in for loop (ch8) - styling

--- rust-book/c8-common-collections/src/exercises/median_and_mode.rs
+++ rust-book/c8-common-collections/src/exercises/median_and_mode.rs
@@ -24,13 +24,13 @@ fn mode(numbers: &Vec<i32>) -> (i32, i32){
 
   let mut occurences: HashMap<i32, i32> = HashMap::new();
 
-  for n in numbers {
-    let occurences_number_n = occurences.entry(*n).or_insert(0);
+  for &n in numbers {
+    let occurences_number_n = occurences.entry(n).or_insert(0);
     *occurences_number_n += 1;
 
     if *occurences_number_n > mode_occurrences {
       mode_occurrences = *occurences_number_n;
-      mode = *n;
+      mode = n;
     }
   }
 
@@ -40,7 +40,7 @@ fn mode(numbers: &Vec<i32>) -> (i32, i32){
 pub fn calculate_median_and_mode(numbers: &Vec<i32>) {
   let mut sorted_numbers = (*numbers).clone();
   sorted_numbers.sort();
-  
+
   let median = median(&sorted_numbers);
   println!("median({numbers:?}) = {median}");
   let (mode, mode_occurrences) = mode(&sorted_numbers);
